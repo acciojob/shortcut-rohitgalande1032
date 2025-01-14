3ce3eea227a9242772d4ff194e89fa32c77193ef
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-14 17:42:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function shortcut(s1, s2) {
   // your code here
-	if(str1.length > 1 && str2.length>1){
-      return (str1[0]+str2[0]);
-    } else {
-      return '';
+	if (!str1 || !str2) {
+        return "''";
     }
+
+    return `'${str1[0]}${str2[0]}'`;
 }
 
 // Do not change the code below.
